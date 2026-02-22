8982793df4be3fb1e991d13ea6bc8524f1ec16f8
Lepotototor <victor.flament@epita.fr>
2026-02-23 00:06:08 +0100

    add special settings for .cc, .hh and .hxx files

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -17,21 +17,21 @@ vim.cmd.colorscheme("rose-pine-moon")
 -- vim.o.autoindent = true
 
 
-vim.o.tabstop = 4      -- Un caractère TAB ressemble à 4 espaces
-vim.o.softtabstop = 4  -- Nombre d'espaces insérés à la place d'un caractère TAB
-vim.o.shiftwidth = 4   -- Nombre d'espaces insérés lors de l'indentation
+vim.o.tabstop = 4       -- Un caractère TAB ressemble à 4 espaces
+vim.o.softtabstop = 4   -- Nombre d'espaces insérés à la place d'un caractère TAB
+vim.o.shiftwidth = 4    -- Nombre d'espaces insérés lors de l'indentation
 vim.o.expandtab = false -- Appuyer sur la touche TAB insérera des espaces au lieu d'un caractère TAB
-vim.opt.cindent = true -- override indent en c
+vim.opt.cindent = true  -- override indent en c
 
 -- s'assurer que pour les buffers C/C++ les options sont bien appliquées (buffer-local)
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = {"c", "cpp", "objc", "h", "hpp"},
-  callback = function(args)
-    local b = vim.bo[args.buf]
-    b.tabstop = 4
-    b.shiftwidth = 4
-    b.softtabstop = 4
-    b.expandtab = true
-    b.cindent = true
-  end,
+	pattern = { "c", "cc", "cpp", "objc", "h", "hh", "hpp", "hxx" },
+	callback = function(args)
+		local b = vim.bo[args.buf]
+		b.tabstop = 4
+		b.shiftwidth = 4
+		b.softtabstop = 4
+		b.expandtab = true
+		b.cindent = true
+	end,
 })
